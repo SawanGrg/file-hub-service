180c43ade612862f038eeb52fe1f407fa57b3ad5
SawanGurung <grgsawan123@gmail.com>
2025-07-01 15:10:24 +0545

    init

---
+++ src/main/java/dev/sawan/filehub/FileHubServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.sawan.filehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileHubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileHubServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sawan/filehub/FileHubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sawan.filehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileHubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
