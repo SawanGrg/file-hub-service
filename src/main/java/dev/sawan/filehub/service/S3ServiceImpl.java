0bb6b120e9c2149986b4cd9b61bb8eb27db180d5
SawanGurung <grgsawan123@gmail.com>
2025-08-19 18:47:57 +0545

    chore: delete object from s3 bucket

--- src/main/java/dev/sawan/filehub/service/S3ServiceImpl.java
+++ src/main/java/dev/sawan/filehub/service/S3ServiceImpl.java
@@ -1,13 +1,13 @@
 package dev.sawan.filehub.service;
 
+import dev.sawan.filehub.config.S3Config;
 import dev.sawan.filehub.config.S3Properties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.IOException;
 
@@ -28,24 +28,45 @@ public class S3ServiceImpl implements S3Service {
             String bucketName = s3Properties.getBucketName();
             String fileName = file.getOriginalFilename();
 
-            // Build the PUT request (not the client!)
+            // Validate file name
+            if (fileName == null || fileName.isEmpty()) {
+                throw new RuntimeException("File name cannot be null or empty");
+            }
+
             PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                     .bucket(bucketName)
                     .key(fileName)
                     .contentType(file.getContentType())
                     .build();
 
-            // Use the S3Client to execute the request
-            PutObjectResponse response = s3Client.putObject(
+            s3Client.putObject(
                     putObjectRequest,
                     RequestBody.fromInputStream(file.getInputStream(), file.getSize())
             );
 
-            // Return the S3 URL or key
-            return String.format("https://%s.s3.amazonaws.com/%s", bucketName, fileName);
+            // Return the S3 URL
+            return String.format("https://%s.s3.%s.amazonaws.com/%s",
+                    bucketName, s3Properties.getRegion(), fileName);
 
         } catch (IOException e) {
             throw new RuntimeException("Failed to upload file to S3", e);
+        }
+    }
+
+    public boolean delete(String fileName) {
+        try {
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                    .bucket(s3Properties.getBucketName())
+                    .key(fileName)
+                    .build();
+
+            s3Client.deleteObject(deleteObjectRequest);
+            return true; // Success
+
+        } catch (S3Exception e) {
+            // Log the error (consider using a proper logger)
+            System.err.println("Failed to delete file from S3: " + e.getMessage());
+            return false; // Failure
         }
     }
 }
